0c0d0df6841956cc006941c62aeafc3af9e88194
vagrant <vagrant@localhost.localdomain>
2020-03-27 12:32:58 +0000

    [Finish] RT8

--- sample_app/test/integration/users_signup_test.rb
+++ sample_app/test/integration/users_signup_test.rb
@@ -13,10 +13,21 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
-    follow_redirect!
-    assert_template 'users/show'
+    assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.alert'
-    assert_not flash.empty?
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
